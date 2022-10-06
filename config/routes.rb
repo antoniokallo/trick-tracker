74331b909104d543b758d23f2c902d8038fc43f6
Danny Lucas <lannyducas@gmail.com>
2022-10-06 18:42:09 -0400

    login backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :skater_tricks
   resources :tricks
   resources :skaters
-  resources :users
+  resources :users, only: [:new, :create, :index, :show]
+
+  post '/login', to: 'users#login'
+  post "/profile", to: 'users#profile'
 end
