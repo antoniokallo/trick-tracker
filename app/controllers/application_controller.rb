4e72b9939cb3a1ad63e539249afca56174784f44
Danny Lucas <lannyducas@gmail.com>
2022-10-06 21:48:34 -0400

    backend fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
 class ApplicationController < ActionController::API
-  before_action :require_login
-  helper_method :current_user
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  def require_login
-    redirect_to new_session_path unless session.include? :user_id
-  end
+    def get_secret_key
+      "123"
+    end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+    def generate_token(user_id)
+      JWT.encode({user_id:user_id}, get_secret_key)
+    end
 
+    def decode_token(token)
+      JWT.decode(token, get_secret_key)[0]["user_id"]
+    end
+
+    private
+
+    def render_not_found_response(exception)
+      render json: { error: "#{exception.model} not found" }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
